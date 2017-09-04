9cc7153b97a4900950bd87442f66eaa134d84857
student <student@PARIS3-2.bt-training.be>
2017-09-04 12:32:22 +0200

    init commit

---
+++ TestGitEclipse-1/src/main/java/be/businesstraining/TestGitEclipse_1/App.java
@@ -0,0 +1,18 @@
+package be.businesstraining.TestGitEclipse_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        showLogginFramework();
+    }
+
+	private static void showLogginFramework() {
+		System.out.println("hola chu");
+	}
+}
